468e802a32077b4c012125fb383b0e76a5db3ee5
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-02-07 01:06:17 +0900

    change table to ul and polish loop prosess

--- src/main/resources/static/js/todo.js
+++ src/main/resources/static/js/todo.js
@@ -1,12 +1,8 @@
 const url = new URL(location.href);
 const id = url.searchParams.get('id');
-const todoId = document.getElementById('todoId');
-const todoActivityName = document.getElementById('todoActivityName');
-const todoCategory = document.getElementById('todoCategory');
-const todoLabel = document.getElementById('todoLabel');
-const todoProgress = document.getElementById('todoProgress');
 const deleteButton = document.getElementById('delete');
 const h1 = document.getElementById('h1');
+const todoUl = document.getElementById('todo');
 
 fetch(url.origin + '/api/todo/' + id)
     .then(res => {
@@ -16,27 +12,12 @@ fetch(url.origin + '/api/todo/' + id)
         throw new Error("fetch failure...");
     })
     .then(todo => {
-        console.log(todo);
-        // TODO: attribute を順に取り出して処理とかできそう
-        const idTd = document.createElement('td');
-        idTd.append(todo.id);
-        todoId.append(idTd);
-
-        const activityNameTd = document.createElement('td');
-        activityNameTd.append(todo.activityName);
-        todoActivityName.append(activityNameTd);
-
-        const progressTd = document.createElement('td');
-        progressTd.append(todo.progress);
-        todoProgress.append(progressTd);
-
-        const categoryTd = document.createElement('td');
-        categoryTd.append(todo.category);
-        todoCategory.append(categoryTd);
-
-        const labelTd = document.createElement('td');
-        labelTd.append(todo.label);
-        todoLabel.append(labelTd);
+        Object.keys(todo).forEach(key => {
+            const li = document.createElement('li');
+            const todoValue = todo[key];
+            li.append(key + ":" + todoValue);
+            todoUl.append(li);
+        })
     })
     .catch(error => {
         console.log("error");

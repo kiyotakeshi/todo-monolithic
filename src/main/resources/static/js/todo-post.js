d1bbd0d3698b16f8bca41fd83eede0221770caf4
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-02-07 23:15:50 +0900

    post new todo

--- src/main/resources/static/js/todo-post.js
+++ src/main/resources/static/js/todo-post.js
@@ -1,16 +1,37 @@
-var myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+const form = document.getElementById('register');
 
-var raw = JSON.stringify({"activityName":"test","progress":"Doing","category":"None"});
+const postTodo = () => {
+    const formData = new FormData(form);
+    formData.append("progress", "Doing");
+    // @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
+    const plainFormData = Object.fromEntries(formData.entries());
+    console.log(plainFormData);
 
-var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+    // sample data
+    // JSON.stringify({activityName: "test", category: "None", label: "aaaa", progress: "Doing"});
+    const data = JSON.stringify(plainFormData);
+    console.log(data);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: data,
+        redirect: 'follow'
+    };        
+
+    fetch(window.location.origin + "/api/todo/", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        console.log(result);
+        console.log("submit!!!");
+    })
+    .catch(error => console.log('error', error));
 };
 
-fetch("http://localhost:8081/api/todo/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    postTodo();
+});        

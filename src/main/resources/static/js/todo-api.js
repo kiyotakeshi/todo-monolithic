afd05ad175eb91f2b929b0d05ca6c317d4256197
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-02-26 02:48:06 +0900

    add index page layout(font awesome)

--- src/main/resources/static/js/todo-api.js
+++ src/main/resources/static/js/todo-api.js
@@ -17,10 +17,15 @@ class TodoApi {
         })
         .then((todoList) => {
             todoList.forEach((todo) => {
+                // sample
+                // <a href="http://localhost:8081/detail?id=1001"> <i class="fas fa-pen-square"></i> 1001: listen to music </a>
                 const li = document.createElement('li');
                 const a = document.createElement('a');
                 a.href = location.origin + '/detail?id=' + todo.id;
                 a.innerText = `${todo.id}: ${todo.activityName}`;
+                const i = document.createElement('i');
+                i.classList.add('fas', 'fa-pen-square');
+                a.prepend(i);
                 li.append(a);
                 todoUl.append(li);
             });
